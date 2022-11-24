4eac6da64f3e80def1bd68a3751a4c6fad7cb923
Sameerijaz112 <sameerejaz970@gmail.com>
2022-11-24 12:14:12 +0500

    sdas

--- index.js
+++ index.js
@@ -10,3 +10,9 @@ x = 5
 y = 5
 z = x+y
 document.getElementById('value').innerHTML = 'The value of z is' + z + "."; */
+const A = {type:'value', model:'121', color:'white'};
+A.color = 'red';
+A.owner = 'john';
+function changeowner(params) {
+    document.getElementById("demo").innerHTML = "Car owner is " + A.owner; 
+}
